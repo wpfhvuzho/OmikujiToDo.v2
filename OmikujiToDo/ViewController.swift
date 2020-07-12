8717e24d4d37dfa18f4c018be5729084862cf66f
sir <terror.neru3@gmail.com>
2020-07-12 15:38:40 +0900

    Initial Commit

---
+++ OmikujiToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OmikujiToDo
+//
+//  Created by 高橋　龍 on 2020/07/12.
+//  Copyright © 2020 Takahashi sir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
